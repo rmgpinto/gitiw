4de4b6672d80d16843563abde5a7ae767b49fdf2
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-01-20 18:40:45 +0000

    Add memorystore terraform module

---
+++ .terraform/modules/memorystore/memorystore.tf
@@ -0,0 +1,7 @@
+resource "google_redis_instance" "redis_instance" {
+  name           = var.name
+  location_id    = "${var.region}-a"
+  tier           = "BASIC"
+  memory_size_gb = var.memory_size_gb
+  redis_version  = var.version
+}
---
+++ .terraform/modules/memorystore/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  description = "Redis name"
+}
+
+variable "region" {
+  description = "Google region"
+}
+
+variable "version" {
+  description = "Redis version"
+}
+
+variable "memory_size_gb" {
+  description = "Redis memory size"
+}
