58d700a8a83b4da8a7094c5a3cbddef198846adf
Ricardo Gândara Pinto <hessian_loom_0u@icloud.com>
2024-01-21 08:11:15 +0000

    Auto-decode redis responses

--- src/modules/redis_client.py
+++ src/modules/redis_client.py
@@ -6,7 +6,7 @@ REDIS_CLIENT = None
 
 
 def init():
-  globals()["REDIS_CLIENT"] = redis.Redis(host=os.getenv("REDIS_HOST"), port=6379)
+  globals()["REDIS_CLIENT"] = redis.Redis(host=os.getenv("REDIS_HOST"), port=6379, charset="utf-8", decode_responses=True)
 
 
 def client():
@@ -15,7 +15,7 @@ def client():
 
 def send_webhook_to_stream(stream, payload):
   response = client().xadd(stream, payload)
-  if type(response) == bytes and len(response.decode("utf-8").split("-")) == 2:
+  if type(response) == str and len(response.split("-")) == 2:
     return True
   else:
     return False
